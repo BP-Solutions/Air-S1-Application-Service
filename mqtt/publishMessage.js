286592cfe51fa72cd24ded47e1dda9abbecd43b1
Marc Hyeler <birdpump@gmail.com>
2024-06-10 00:42:57 -0700

    update qos config

--- mqtt/publishMessage.js
+++ mqtt/publishMessage.js
@@ -5,7 +5,7 @@ export async function publishMessage(msg) {
 
     const topic = `${globalConfig.topic}`;
 
-    client.publish(topic, msgPayload, { qos: 2, retain: true },(err) => {
+    client.publish(topic, msgPayload, {qos: 1, retain: true, dup: true},(err) => {
         if (err) {
             console.error('Failed to publish message:', err);
             throw err;
