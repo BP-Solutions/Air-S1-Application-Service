cfcfebbad30b5e2040546ad3ee5e3a05158d470a
Marc Hyeler <birdpump@gmail.com>
2024-08-04 15:07:42 -0700

    test background calibration

--- tools/calibrate.js
+++ tools/calibrate.js
@@ -1,14 +1,14 @@
 import {encodeSerial} from "../protobufs/proto.js";
 import {port} from "../main.js";
 
-let payload = {statCode: 0, inErrorState: false, sampleSensors: false, configSXX: true, abcPer: 180, SXXCalType: 0};
+let payload = {statCode: 0, inErrorState: false, sampleSensors: false, configSXX: true, abcPer: 0, SXXCalType: 2};
 
 let hexString = await encodeSerial(payload);
 
 let message = `\nR:${hexString}\n`;
+
 port.write(message, (err) => {
     if (err) {
         return console.error('Error on write:', err.message);
     }
 });
-return;
