bcdf8d687d9bf9a3d815ccbfd477c5b8ced68ce6
Marc Hyeler <birdpump@gmail.com>
2024-08-04 15:05:27 -0700

    revert serialport

--- tools/calibrate.js
+++ tools/calibrate.js
@@ -1,32 +1,13 @@
 import {encodeSerial} from "../protobufs/proto.js";
+import {port} from "../main.js";
 
-import {SerialPort} from "serialport";
-import {ReadlineParser} from "@serialport/parser-readline";
-import {callData} from "../serializers/serialize.js";
+let payload = {statCode: 0, inErrorState: false, sampleSensors: false, configSXX: true, abcPer: 180, SXXCalType: 0};
 
-const port = new SerialPort({
-    path: '/dev/myserialdevice',
-    baudRate: 115200
-});
+let hexString = await encodeSerial(payload);
 
-const parser = port.pipe(new ReadlineParser({delimiter: '\r\n'}));
-
-port.on('open', async () => {
-    console.log('Serial Port Opened');
-
-    let payload = {statCode: 0, inErrorState: false, sampleSensors: false, configSXX: true, abcPer: 180, SXXCalType: 0};
-
-    let hexString = await encodeSerial(payload);
-
-    let message = `\nR:${hexString}\n`;
-    port.write(message, (err) => {
-        if (err) {
-            return console.error('Error on write:', err.message);
-        }
-    });
-
-});
-
-port.on('error', (err) => {
-    console.error('Serial Error:', err.message);
+let message = `\nR:${hexString}\n`;
+port.write(message, (err) => {
+    if (err) {
+        return console.error('Error on write:', err.message);
+    }
 });
